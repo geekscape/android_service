03e658c33b09b2e8a6be471e87045fea7940bdf2
Andy Gelme <andyg@geekscape.org>
2011-11-27 18:01:20 +1100

    Begin adding the OpenADK USB implementation.

---
+++ src/org/geekscape/android/androidservice/UsbAccessoryActivity.java
@@ -0,0 +1,22 @@
+package org.geekscape.android.androidservice;
+
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+public final class UsbAccessoryActivity extends Activity {
+
+  static final String LOG_TAG = UsbAccessoryActivity.class.getSimpleName();
+
+	@Override
+	protected void onCreate(
+    Bundle savedInstanceState) {
+
+    super.onCreate(savedInstanceState);
+    Log.d(LOG_TAG, "onCreate()");
+
+    startService(new Intent(AndroidService.class.getName()));
+  }
+}
